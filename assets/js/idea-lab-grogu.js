c420e0c4196b57d67b8f3f3ec50f907fed4fba5f
Kim <5739370+kim-codes@users.noreply.github.com>
2026-07-24 18:44:19 -0400

    Update to allow dragging  idea-lab-grogu.js

--- assets/js/idea-lab-grogu.js
+++ assets/js/idea-lab-grogu.js
@@ -8,11 +8,13 @@ const svg = document.querySelector(".galaxy-map");
 
 const nodes = document.querySelectorAll(".character");
 
+/*
 console.log(beacon); 
 
 beacon.addEventListener("click", () => {
     console.log("Beacon clicked");
 });
+*/
 
 // test - beacon.style.stroke = "#8ccf6e"; 
 
@@ -20,14 +22,40 @@ beacon.addEventListener("click", () => {
    State
 -------------------------- */
 
-let isDragging = false;
+// allow user to start dragging
+beacon.addEventListener("mousedown", () => {
+    isDragging = true;
+});
 
+// Stop dragging
+document.addEventListener("mouseup", () => {
+    isDragging = false;
+});
 
+// Move the beacon
+svg.addEventListener("mousemove", (event) => {
+
+    if (!isDragging) return;
+
+    const point = svg.createSVGPoint();
+
+    point.x = event.clientX;
+    point.y = event.clientY;
+
+    const svgPoint = point.matrixTransform(
+        svg.getScreenCTM().inverse()
+    );
+
+    beacon.setAttribute("cx", svgPoint.x);
+    beacon.setAttribute("cy", svgPoint.y);
+
+});
 
 /* --------------------------
    Event Listeners
 -------------------------- */
 
+/*
 beacon.addEventListener("mousedown", () => {
     isDragging = true;
     console.log("Dragging started");
@@ -36,7 +64,7 @@ beacon.addEventListener("mousedown", () => {
 document.addEventListener("mouseup", () => {
     isDragging = false;
     console.log("Dragging stopped");
-});
+});*/
 
 /* --------------------------
    Functions

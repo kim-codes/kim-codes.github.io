7c72b6aaa1d7558282fd94cc3a5fe012cc514a7b
Kim <5739370+kim-codes@users.noreply.github.com>
2026-07-24 16:25:24 -0400

    Create idea-lab-grogu.js

---
+++ assets/js/idea-lab-grogu.js
@@ -0,0 +1,35 @@
+console.log("Idea Lab loaded!");
+
+/* --------------------------
+   Elements
+-------------------------- */
+const beacon = document.getElementById("search-beacon");
+const nodes = document.querySelectorAll(".character");
+
+console.log("Closest:", "Din Djarin");
+
+/* --------------------------
+   State
+-------------------------- */
+
+let isDragging = false;
+
+
+
+/* --------------------------
+   Event Listeners
+-------------------------- */
+
+
+
+/* --------------------------
+   Functions
+-------------------------- */
+
+function updateNearestNeighbors() {
+
+}
+
+function moveBeacon() {
+
+}
